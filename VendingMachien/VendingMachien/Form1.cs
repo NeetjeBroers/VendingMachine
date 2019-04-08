63cd417c852df19a6c64987a6b57571f49ef75fd
NeetjeBroers <nathan.broers2000@gmail.com>
2019-04-08 11:32:08 +0200

    Form Rename Form Real

--- VendingMachien/VendingMachien/Form1.cs
+++ VendingMachien/VendingMachien/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VendingMachien
 {
-    public partial class Form1 : Form
+    public partial class FormVendingMachine : Form
     {
-        public Form1()
+        public FormVendingMachine()
         {
             InitializeComponent();
         }

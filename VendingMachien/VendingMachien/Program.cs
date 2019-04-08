f7bb5c13c7f8691153751f54af845e7af1d07c71
NeetjeBroers <nathan.broers2000@gmail.com>
2019-04-08 11:29:37 +0200

    Form Rename

--- VendingMachien/VendingMachien/Program.cs
+++ VendingMachien/VendingMachien/Program.cs
@@ -16,7 +16,7 @@ namespace VendingMachien
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormVendingMachine());
         }
     }
 }

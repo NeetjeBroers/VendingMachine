4229207e5541ce51c62422aeb13a8193a4fe397d
NeetjeBroers <nathan.broers2000@gmail.com>
2019-04-08 11:20:07 +0200

    Update Form1.Designer.cs

--- VendingMachine/Form1.Designer.cs
+++ VendingMachine/Form1.Designer.cs
@@ -28,16 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelFakkaGlenn = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelFakkaGlenn
+            // 
+            this.labelFakkaGlenn.AutoSize = true;
+            this.labelFakkaGlenn.Location = new System.Drawing.Point(354, 165);
+            this.labelFakkaGlenn.Name = "labelFakkaGlenn";
+            this.labelFakkaGlenn.Size = new System.Drawing.Size(68, 13);
+            this.labelFakkaGlenn.TabIndex = 0;
+            this.labelFakkaGlenn.Text = "Fakka Glenn";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelFakkaGlenn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 

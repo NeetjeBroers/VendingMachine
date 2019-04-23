d1b5e7f8365b3a34bc2518a14573412ce9fdef62
Glenn Nellensteijn <400028028@st.roc.a12.nl>
2019-04-23 11:21:41 +0200

    Fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1061, 439);
+            this.button1.Location = new System.Drawing.Point(1136, 393);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(21, 185);
             this.button1.TabIndex = 2;
@@ -64,7 +64,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(1281, 692);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);

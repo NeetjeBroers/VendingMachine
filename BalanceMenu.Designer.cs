28443ed80395767d39c6327f98559b66e06a0e1c
Glenn Nellensteijn <400028028@st.roc.a12.nl>
2019-04-25 12:00:10 +0200

    Update BalanceMenu.Designer.cs

--- BalanceMenu.Designer.cs
+++ BalanceMenu.Designer.cs
@@ -43,15 +43,15 @@
             // buttonRefundBalance
             // 
             this.buttonRefundBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(165)))), ((int)(((byte)(216)))));
-            this.buttonRefundBalance.Cursor = System.Windows.Forms.Cursors.No;
+            this.buttonRefundBalance.Cursor = System.Windows.Forms.Cursors.Default;
             this.buttonRefundBalance.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.buttonRefundBalance.FlatAppearance.BorderSize = 5;
             this.buttonRefundBalance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonRefundBalance.Font = new System.Drawing.Font("Trebuchet MS", 14F, System.Drawing.FontStyle.Bold);
-            this.buttonRefundBalance.Location = new System.Drawing.Point(401, 439);
+            this.buttonRefundBalance.Location = new System.Drawing.Point(602, 675);
             this.buttonRefundBalance.Margin = new System.Windows.Forms.Padding(0);
             this.buttonRefundBalance.Name = "buttonRefundBalance";
-            this.buttonRefundBalance.Size = new System.Drawing.Size(110, 71);
+            this.buttonRefundBalance.Size = new System.Drawing.Size(165, 109);
             this.buttonRefundBalance.TabIndex = 1;
             this.buttonRefundBalance.Text = "Refund Balance";
             this.buttonRefundBalance.UseVisualStyleBackColor = false;
@@ -60,9 +60,10 @@
             // 
             this.labelCurrentBalance.AutoSize = true;
             this.labelCurrentBalance.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCurrentBalance.Location = new System.Drawing.Point(364, 9);
+            this.labelCurrentBalance.Location = new System.Drawing.Point(546, 14);
+            this.labelCurrentBalance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCurrentBalance.Name = "labelCurrentBalance";
-            this.labelCurrentBalance.Size = new System.Drawing.Size(113, 35);
+            this.labelCurrentBalance.Size = new System.Drawing.Size(167, 49);
             this.labelCurrentBalance.TabIndex = 2;
             this.labelCurrentBalance.Text = "Balance";
             // 
@@ -70,9 +71,10 @@
             // 
             this.labelCurrentBalanceValue.AutoSize = true;
             this.labelCurrentBalanceValue.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCurrentBalanceValue.Location = new System.Drawing.Point(494, 9);
+            this.labelCurrentBalanceValue.Location = new System.Drawing.Point(741, 14);
+            this.labelCurrentBalanceValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCurrentBalanceValue.Name = "labelCurrentBalanceValue";
-            this.labelCurrentBalanceValue.Size = new System.Drawing.Size(31, 35);
+            this.labelCurrentBalanceValue.Size = new System.Drawing.Size(45, 49);
             this.labelCurrentBalanceValue.TabIndex = 3;
             this.labelCurrentBalanceValue.Text = "0";
             // 
@@ -80,17 +82,17 @@
             // 
             this.buttonFiveCent.BackColor = System.Drawing.Color.Transparent;
             this.buttonFiveCent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.buttonFiveCent.Cursor = System.Windows.Forms.Cursors.No;
+            this.buttonFiveCent.Cursor = System.Windows.Forms.Cursors.Default;
             this.buttonFiveCent.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.buttonFiveCent.FlatAppearance.BorderSize = 0;
             this.buttonFiveCent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonFiveCent.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonFiveCent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
             this.buttonFiveCent.Image = ((System.Drawing.Image)(resources.GetObject("buttonFiveCent.Image")));
-            this.buttonFiveCent.Location = new System.Drawing.Point(9, 9);
+            this.buttonFiveCent.Location = new System.Drawing.Point(14, 14);
             this.buttonFiveCent.Margin = new System.Windows.Forms.Padding(0);
             this.buttonFiveCent.Name = "buttonFiveCent";
-            this.buttonFiveCent.Size = new System.Drawing.Size(112, 124);
+            this.buttonFiveCent.Size = new System.Drawing.Size(168, 191);
             this.buttonFiveCent.TabIndex = 4;
             this.buttonFiveCent.Text = "0.05";
             this.buttonFiveCent.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -102,17 +104,17 @@
             // 
             this.buttonFiftyCent.BackColor = System.Drawing.Color.Transparent;
             this.buttonFiftyCent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.buttonFiftyCent.Cursor = System.Windows.Forms.Cursors.No;
+            this.buttonFiftyCent.Cursor = System.Windows.Forms.Cursors.Default;
             this.buttonFiftyCent.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.buttonFiftyCent.FlatAppearance.BorderSize = 0;
             this.buttonFiftyCent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonFiftyCent.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonFiftyCent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
             this.buttonFiftyCent.Image = ((System.Drawing.Image)(resources.GetObject("buttonFiftyCent.Image")));
-            this.buttonFiftyCent.Location = new System.Drawing.Point(21, 133);
+            this.buttonFiftyCent.Location = new System.Drawing.Point(32, 205);
             this.buttonFiftyCent.Margin = new System.Windows.Forms.Padding(0);
             this.buttonFiftyCent.Name = "buttonFiftyCent";
-            this.buttonFiftyCent.Size = new System.Drawing.Size(100, 130);
+            this.buttonFiftyCent.Size = new System.Drawing.Size(150, 200);
             this.buttonFiftyCent.TabIndex = 7;
             this.buttonFiftyCent.Text = "0.50";
             this.buttonFiftyCent.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -123,17 +125,17 @@
             // 
             this.buttonTenCent.BackColor = System.Drawing.Color.Transparent;
             this.buttonTenCent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.buttonTenCent.Cursor = System.Windows.Forms.Cursors.No;
+            this.buttonTenCent.Cursor = System.Windows.Forms.Cursors.Default;
             this.buttonTenCent.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.buttonTenCent.FlatAppearance.BorderSize = 0;
             this.buttonTenCent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonTenCent.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonTenCent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
             this.buttonTenCent.Image = ((System.Drawing.Image)(resources.GetObject("buttonTenCent.Image")));
-            this.buttonTenCent.Location = new System.Drawing.Point(187, 16);
+            this.buttonTenCent.Location = new System.Drawing.Point(280, 25);
             this.buttonTenCent.Margin = new System.Windows.Forms.Padding(0);
             this.buttonTenCent.Name = "buttonTenCent";
-            this.buttonTenCent.Size = new System.Drawing.Size(86, 111);
+            this.buttonTenCent.Size = new System.Drawing.Size(129, 171);
             this.buttonTenCent.TabIndex = 9;
             this.buttonTenCent.Text = "0.10";
             this.buttonTenCent.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -144,17 +146,17 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.Cursor = System.Windows.Forms.Cursors.No;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(187, 138);
+            this.button1.Location = new System.Drawing.Point(280, 212);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 120);
+            this.button1.Size = new System.Drawing.Size(140, 185);
             this.button1.TabIndex = 10;
             this.button1.Text = "0.20";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -165,17 +167,17 @@
             // 
             this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.Cursor = System.Windows.Forms.Cursors.No;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
             this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(28, 292);
+            this.button2.Location = new System.Drawing.Point(42, 449);
             this.button2.Margin = new System.Windows.Forms.Padding(0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 124);
+            this.button2.Size = new System.Drawing.Size(140, 191);
             this.button2.TabIndex = 11;
             this.button2.Text = "1";
             this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -186,17 +188,17 @@
             // 
             this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button3.Cursor = System.Windows.Forms.Cursors.No;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
             this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(187, 292);
+            this.button3.Location = new System.Drawing.Point(280, 449);
             this.button3.Margin = new System.Windows.Forms.Padding(0);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 134);
+            this.button3.Size = new System.Drawing.Size(162, 206);
             this.button3.TabIndex = 12;
             this.button3.Text = "2";
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -205,10 +207,10 @@
             // 
             // BalanceMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
-            this.ClientSize = new System.Drawing.Size(560, 545);
+            this.ClientSize = new System.Drawing.Size(840, 838);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -218,6 +220,7 @@
             this.Controls.Add(this.labelCurrentBalanceValue);
             this.Controls.Add(this.labelCurrentBalance);
             this.Controls.Add(this.buttonRefundBalance);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "BalanceMenu";
             this.Text = "Balance Menu";
             this.ResumeLayout(false);

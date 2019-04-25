c13cb9909770993d0f5631b7ffcd028b977b0515
Glenn Nellensteijn <400028028@st.roc.a12.nl>
2019-04-25 10:24:56 +0200

    Design update

--- BalanceMenu.Designer.cs
+++ BalanceMenu.Designer.cs
@@ -36,49 +36,62 @@
             // 
             // buttonRefundBalance
             // 
-            this.buttonRefundBalance.Location = new System.Drawing.Point(413, 462);
+            this.buttonRefundBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(165)))), ((int)(((byte)(216)))));
+            this.buttonRefundBalance.Cursor = System.Windows.Forms.Cursors.No;
+            this.buttonRefundBalance.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonRefundBalance.FlatAppearance.BorderSize = 5;
+            this.buttonRefundBalance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRefundBalance.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRefundBalance.Location = new System.Drawing.Point(620, 711);
+            this.buttonRefundBalance.Margin = new System.Windows.Forms.Padding(0);
             this.buttonRefundBalance.Name = "buttonRefundBalance";
-            this.buttonRefundBalance.Size = new System.Drawing.Size(110, 71);
+            this.buttonRefundBalance.Size = new System.Drawing.Size(165, 109);
             this.buttonRefundBalance.TabIndex = 1;
             this.buttonRefundBalance.Text = "Refund Balance";
-            this.buttonRefundBalance.UseVisualStyleBackColor = true;
+            this.buttonRefundBalance.UseVisualStyleBackColor = false;
             // 
             // labelCurrentBalance
             // 
             this.labelCurrentBalance.AutoSize = true;
-            this.labelCurrentBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.labelCurrentBalance.Location = new System.Drawing.Point(364, 9);
+            this.labelCurrentBalance.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentBalance.Location = new System.Drawing.Point(546, 14);
+            this.labelCurrentBalance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCurrentBalance.Name = "labelCurrentBalance";
-            this.labelCurrentBalance.Size = new System.Drawing.Size(112, 31);
+            this.labelCurrentBalance.Size = new System.Drawing.Size(167, 49);
             this.labelCurrentBalance.TabIndex = 2;
             this.labelCurrentBalance.Text = "Balance";
             // 
             // labelCurrentBalanceValue
             // 
             this.labelCurrentBalanceValue.AutoSize = true;
-            this.labelCurrentBalanceValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.labelCurrentBalanceValue.Location = new System.Drawing.Point(494, 9);
+            this.labelCurrentBalanceValue.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentBalanceValue.Location = new System.Drawing.Point(741, 14);
+            this.labelCurrentBalanceValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCurrentBalanceValue.Name = "labelCurrentBalanceValue";
-            this.labelCurrentBalanceValue.Size = new System.Drawing.Size(29, 31);
+            this.labelCurrentBalanceValue.Size = new System.Drawing.Size(45, 49);
             this.labelCurrentBalanceValue.TabIndex = 3;
             this.labelCurrentBalanceValue.Text = "0";
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 9);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(165)))), ((int)(((byte)(216)))));
+            this.panel1.Location = new System.Drawing.Point(18, 14);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(346, 524);
+            this.panel1.Size = new System.Drawing.Size(519, 806);
             this.panel1.TabIndex = 4;
             // 
             // BalanceMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 545);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
+            this.ClientSize = new System.Drawing.Size(840, 838);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.labelCurrentBalanceValue);
             this.Controls.Add(this.labelCurrentBalance);
             this.Controls.Add(this.buttonRefundBalance);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "BalanceMenu";
             this.Text = "Balance Menu";
             this.ResumeLayout(false);
--- VendingMachien.Designer.cs
+++ VendingMachien.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,27 +45,42 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1123, 392);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Khaki;
+            this.button1.FlatAppearance.BorderSize = 4;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(1215, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(21, 185);
             this.button1.TabIndex = 2;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(1058, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(143, 352);
+            this.dataGridView1.Size = new System.Drawing.Size(324, 374);
             this.dataGridView1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1040, 565);
+            this.panel1.TabIndex = 4;
             // 
             // FormVendingMachine
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1224, 655);
+            this.ClientSize = new System.Drawing.Size(1394, 804);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -82,6 +98,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 

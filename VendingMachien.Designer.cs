2f7af0d4f05bf5431b3883abc7227817820d360d
NeetjeBroers <nathan.broers2000@gmail.com>
2019-05-02 11:50:58 +0200

    Update VendingMachien.Designer.cs

--- VendingMachien.Designer.cs
+++ VendingMachien.Designer.cs
@@ -52,7 +52,7 @@
             this.button1.BackColor = System.Drawing.Color.Khaki;
             this.button1.FlatAppearance.BorderSize = 4;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(810, 255);
+            this.button1.Location = new System.Drawing.Point(851, 255);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(14, 120);
@@ -64,7 +64,7 @@
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(705, 8);
+            this.dataGridView1.Location = new System.Drawing.Point(746, 8);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
@@ -76,11 +76,12 @@
             this.panelProduct.Location = new System.Drawing.Point(8, 8);
             this.panelProduct.Margin = new System.Windows.Forms.Padding(2);
             this.panelProduct.Name = "panelProduct";
-            this.panelProduct.Size = new System.Drawing.Size(693, 367);
+            this.panelProduct.Size = new System.Drawing.Size(736, 367);
             this.panelProduct.TabIndex = 4;
             // 
             // buttonAddBalance
             // 
+            this.buttonAddBalance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonAddBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(165)))), ((int)(((byte)(216)))));
             this.buttonAddBalance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAddBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
@@ -97,7 +98,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(111)))), ((int)(((byte)(164)))));
-            this.ClientSize = new System.Drawing.Size(929, 523);
+            this.ClientSize = new System.Drawing.Size(970, 523);
             this.Controls.Add(this.buttonAddBalance);
             this.Controls.Add(this.panelProduct);
             this.Controls.Add(this.dataGridView1);
